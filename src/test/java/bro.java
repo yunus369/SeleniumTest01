221e21371f9c43a76f60f32bd9539f5ee01e6988
M yunus <yunusmsy83@gmail.com>
2024-10-21 07:44:11 +0530

    new code

---
+++ src/test/java/bro.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.List;
+
+public class bro {
+    public static void main(String[] args) throws IOException {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+
+        List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+
+        SoftAssert a = new SoftAssert();
+
+
+        for (WebElement link : links) {
+
+
+            String url = link.getAttribute("href");
+
+
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            conn.setRequestMethod("HEAD");
+
+            conn.connect();
+
+            int respCode = conn.getResponseCode();
+
+            System.out.println(respCode);
+
+            a.assertTrue(respCode < 400, "The link with Text" + link.getText() + " is broken with code" + respCode);
+
+
+        }
+
+
+        a.assertAll();
+    }
+}
